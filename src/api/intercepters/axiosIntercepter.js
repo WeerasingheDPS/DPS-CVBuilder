8c929fa383db1db237401adcde985be911d75b8a
Dulanjana Weerasinghe <87292444+WeerasingheDPS@users.noreply.github.com>
2024-06-14 14:25:41 +0530

    Update axiosIntercepter.js

--- src/api/intercepters/axiosIntercepter.js
+++ src/api/intercepters/axiosIntercepter.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
-axios.defaults.baseURL = "http://65.2.141.117:8090/";
+axios.defaults.baseURL = "http://65.2.141.117:8080/";
 
 
 // Function to refresh the token. Replace this with your actual token refresh logic.
