d74bf7724b14877a2e0365bb9dcff1d947f553b4
WeerasingheDPS <dulanjana.weerasinghe@circles.asia>
2024-06-27 17:05:56 +0530

    change to https

--- src/api/intercepters/axiosIntercepter.js
+++ src/api/intercepters/axiosIntercepter.js
@@ -3,7 +3,7 @@ import { jwtDecode } from 'jwt-decode';
 
 // axios.defaults.baseURL = "http://13.201.84.195:8090/";
 // axios.defaults.baseURL = "http://localhost:8080/api/v1/";
-axios.defaults.baseURL = "http://13.201.16.53:8080/api/v1/";
+axios.defaults.baseURL = "https://13.201.16.53:8080/api/v1/";
 
 
 
